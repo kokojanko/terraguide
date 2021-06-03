ddc208e97871c5c0293a5d65035e7d793b54a3f5
BuildTools <unconfigured@null.spigotmc.org>
2021-06-03 11:25:41 +0300

    scelet of the main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Sidebar from './sidebar/sidebar'
+import './app.css'
+import Worrior from './imege/worrior.png';
+import Wizzard from './imege/wizzard.png';
+import Sommoner from './imege/sommoner.png';
+import Archer from './imege/archer.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='BigWrapper'>
+      <Sidebar />
+      <div className='wrapper'>
+        <div className='container'>
+          <div className="o2t-element">
+          <div>Воин</div>
+          </div>
+          <div className="o2t-element--hidden">
+            <a>
+              <img src={Worrior} width='100%' height='100%' />
+            </a>
+          </div>
+        </div>
+        <div className='container'>
+          <div className="o2t-element">
+            <div>Стрелок</div>
+          </div>
+          <div className="o2t-element--hidden">
+            <a>
+              <img src={Archer} width='120%' height='100%' />
+            </a>
+          </div>
+        </div>
+        <div className='container'>
+          <div className="o2t-element">
+          <div>Маг</div>
+          </div>
+          <div className="o2t-element--hidden">
+            <a>
+              <img src={Wizzard} width='100%' height='100%' />
+            </a>
+          </div>
+        </div>
+        <div className='container'>
+          <div className="o2t-element">
+          <div>Призыватель</div>
+          </div>
+          <div className="o2t-element--hidden">
+            <a>
+              <img src={Sommoner} width='80%' height='80%' />
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <div className="o2t-element"></div>
+
+    <div className="o2t-element--hidden">Скрытый элемент</div> */}
     </div>
+
   );
 }
 
